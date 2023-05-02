2a6b0d9e3fcd589c2183710f7fb2d4fe5cd6aee9
Esdras Gomes Carvalho <esdrasgcdev@gmail.com>
2023-05-02 10:57:42 -0300

    spring initlzr

---
+++ ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/CiclismoPorAmorApplication.java
@@ -0,0 +1,13 @@
+package com.br.ciclismoporamor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiclismoPorAmorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiclismoPorAmorApplication.class, args);
+	}
+
+}
---
+++ ciclismo-por-amor/src/test/java/com/br/ciclismoporamor/CiclismoPorAmorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ciclismoporamor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiclismoPorAmorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
