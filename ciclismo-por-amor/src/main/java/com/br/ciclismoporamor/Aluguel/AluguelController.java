ae811a270f133c68d85f0f0bc9aca33c1fdbf724
esdrasgc <esdrasgcdev@gmail.com>
2023-05-04 09:23:26 -0300

    controller

--- ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/AluguelController.java
+++ ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/AluguelController.java
@@ -1,12 +1,10 @@
 package com.br.ciclismoporamor.Aluguel;
 
+import com.br.ciclismoporamor.Aluguel.dto.DevolveBikeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/aluguel")
@@ -18,6 +16,11 @@ public class AluguelController {
     public Page<Aluguel> listAlugueis(@RequestParam(required = false) String id_bike, Pageable pageable){
         return aluguelService.listarAlugueis(id_bike, pageable);
 
+    }
+
+    @PutMapping("/{identificador}")
+    public Aluguel editAluguel(@PathVariable String identificador, @RequestBody DevolveBikeDTO devolveBikeDTO){
+        return aluguelService.devolverBike(identificador, devolveBikeDTO);
     }
     
     
