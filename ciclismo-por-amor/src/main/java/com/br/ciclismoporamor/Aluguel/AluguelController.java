bc02950669deee18dddf1278c60074c8723f5389
liviatanaka <liviatanaka19@gmail.com>
2023-05-04 09:27:45 -0300

    save aluguel sem id_bike

--- ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/AluguelController.java
+++ ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/AluguelController.java
@@ -1,12 +1,10 @@
 package com.br.ciclismoporamor.Aluguel;
 
+import com.br.ciclismoporamor.Aluguel.dto.SaveAluguelDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/aluguel")
@@ -15,10 +13,15 @@ public class AluguelController {
     private AluguelService aluguelService;
 
     @GetMapping
-    public Page<Aluguel> listAlugueis(@RequestParam(required = false) String id_bike, Pageable pageable){
+    public Page<Aluguel> listAlugueis(@RequestParam(required = false) String id_bike, Pageable pageable) {
         return aluguelService.listarAlugueis(id_bike, pageable);
-
     }
+
+    @PostMapping
+    public Aluguel saveAluguel(@RequestBody SaveAluguelDTO aluguel){
+        return aluguelService.saveAluguel(aluguel);
+    }
+
     
     
 }
---
+++ ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/dto/SaveAluguelDTO.java
@@ -0,0 +1,10 @@
+package com.br.ciclismoporamor.Aluguel.dto;
+
+import lombok.Data;
+
+@Data
+public class SaveAluguelDTO {
+    private String origem;
+    private double[] coordInicial;
+
+}
