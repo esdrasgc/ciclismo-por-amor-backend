1451460f614fd3cc583a03ef207db0dec5affafb
liviatanaka <liviatanaka19@gmail.com>
2023-05-15 17:30:40 -0300

    arrumando documentacao

--- ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/AluguelController.java
+++ ciclismo-por-amor/src/main/java/com/br/ciclismoporamor/Aluguel/AluguelController.java
@@ -3,6 +3,7 @@ package com.br.ciclismoporamor.Aluguel;
 import com.br.ciclismoporamor.Aluguel.dto.InfoAluguelDTO;
 import com.br.ciclismoporamor.Aluguel.dto.SaveAluguelDTO;
 import com.br.ciclismoporamor.Aluguel.dto.DevolveBikeDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,21 +15,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/aluguel")
+@RestControllerAdvice
 public class AluguelController {
     @Autowired
     private AluguelService aluguelService;
 
     @GetMapping
+    @Operation(summary = "Lista todos os aluguéis")
     public List<InfoAluguelDTO> listAlugueis(@RequestParam(required = false) String id_bike, @RequestParam(required = false) AluguelStatus status) {
         return aluguelService.listarAlugueis(id_bike, status);
     }
 
     @PostMapping
+    @Operation(summary = "Salva um novo aluguel")
     public InfoAluguelDTO saveAluguel(@RequestBody SaveAluguelDTO aluguel){
         return aluguelService.saveAluguel(aluguel);
     }
 
     @PutMapping("/{identificador}")
+    @Operation(summary = "Edita um aluguel")
     public Aluguel editAluguel(
             @PathVariable String identificador,
             @RequestBody DevolveBikeDTO devolveBikeDTO
@@ -37,6 +42,7 @@ public class AluguelController {
     }
 
     @DeleteMapping("/{identificador}")
+    @Operation(summary = "Deleta aluguel")
     public void deletAluguel(
             @PathVariable String identificador
     ){
